ccc19aa230dd14a18c3380739936c12f50f25bf7
Danny Xie Li <51223285+dnnxl@users.noreply.github.com>
2019-12-06 18:46:28 -0600

    Init cshop app

---
+++ cshop/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAVrFcpgt2M-Ch_2EJQZOnHRes25NARPp0",
+    authDomain: "cshop-39242.firebaseapp.com",
+    databaseURL: "https://cshop-39242.firebaseio.com",
+    projectId: "cshop-39242",
+    storageBucket: "cshop-39242.appspot.com",
+    messagingSenderId: "305166936187"
+  }
+};
---
+++ cshop/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAVrFcpgt2M-Ch_2EJQZOnHRes25NARPp0",
+    authDomain: "cshop-39242.firebaseapp.com",
+    databaseURL: "https://cshop-39242.firebaseio.com",
+    projectId: "cshop-39242",
+    storageBucket: "cshop-39242.appspot.com",
+    messagingSenderId: "305166936187"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
