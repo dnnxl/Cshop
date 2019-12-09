6f8233fa79105ad76a57275fbf944824c245a01a
Danny Xie Li <51223285+dnnxl@users.noreply.github.com>
2019-12-08 21:53:52 -0600

    Not working

--- cshop/src/app/services/auth.service.ts
+++ cshop/src/app/services/auth.service.ts
@@ -1,8 +1,11 @@
+import { UserService } from './user.service';
 import { Observable } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
 import { ActivatedRoute } from '@angular/router';
+import { AppUser } from '../models/app-user';
+import 'rxjs/add/observable/of';
 
 @Injectable({
   providedIn: 'root'
@@ -11,18 +14,27 @@ export class AuthService {
 
   user$: Observable<firebase.User>;
   
-  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute) { 
+  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute, private userService: UserService) { 
     this.user$ = afAuth.authState;
   }
 
   login(){
     let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
     localStorage.setItem('returnUrl', returnUrl);
-    this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())
-      .then();
+    this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
   }
 
   logout(){
     this.afAuth.auth.signOut();
+  }
+
+  get appUser$(): Observable<AppUser>{
+    return this.user$
+      .switchMap(user => {
+        if(user) return this.userService.get(user.uid).valueChanges();
+
+        return Observable.of(null);
+
+      });
   }
 }
