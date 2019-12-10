5901133451398d8967204c1324155f342f3aa093
Danny Xie Li <51223285+dnnxl@users.noreply.github.com>
2019-12-10 11:17:25 -0600

    Category sticky

--- cshop/src/app/app.module.ts
+++ cshop/src/app/app.module.ts
@@ -29,6 +29,8 @@ import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.componen
 import { LoginComponent } from './login/login.component';
 import { ProductFormComponent } from './admin/product-form/product-form.component';
 import { CategoryService } from './category.service';
+import { ProductFilterComponent } from './products/product-filter/product-filter.component';
+import { ProductCardComponent } from './product-card/product-card.component';
 
 @NgModule({
   declarations: [
@@ -43,7 +45,9 @@ import { CategoryService } from './category.service';
     AdminProductsComponent,
     AdminOrdersComponent,
     LoginComponent,
-    ProductFormComponent
+    ProductFormComponent,
+    ProductFilterComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
--- cshop/src/app/products/products.component.ts
+++ cshop/src/app/products/products.component.ts
@@ -1,6 +1,4 @@
-import { Observable } from 'rxjs/Observable';
 import { ActivatedRoute } from '@angular/router';
-import { CategoryService } from './../category.service';
 import { Component, OnInit, RootRenderer } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import 'rxjs/add/operator/switchMap';
@@ -13,30 +11,30 @@ import 'rxjs/add/operator/switchMap';
 export class ProductsComponent {
 
   products$ = [];
-  categories$;
   category: string;
   filteredProducts;
 
   constructor(
     route: ActivatedRoute,
-    private productService: ProductService, 
-    categoryService: CategoryService) {
+    private productService: ProductService
+  ) {
     //this.products$ = productService.getAll().valueChanges().subscribe;
-    this.categories$ = categoryService.getCategories();
 
     this.productService
       .getAll().snapshotChanges()
       .switchMap(
-      products => {this.products$ = products;
-        return route.queryParamMap;
-      })
+        products => {
+          this.products$ = products;
+          return route.queryParamMap;
+        })
 
-      .subscribe(params =>{
-      this.category = params.get('category');
-      this.filteredProducts = (this.category) ?
-        this.products$.filter(p => p.payload.val().category === this.category) :
-        this.products$;
-    });
-   
+      .subscribe(params => {
+        this.category = params.get('category');
+
+        this.filteredProducts = (this.category) ?
+          this.products$.filter(p => p.payload.val().category === this.category) :
+          this.products$;
+      });
+
   }
 }
