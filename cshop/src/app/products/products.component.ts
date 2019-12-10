5b81e8d1b58ab9a96be402daaac734635a92e9fc
Danny Xie Li <51223285+dnnxl@users.noreply.github.com>
2019-12-09 22:50:06 -0600

    Filtering products by category

--- cshop/src/app/products/products.component.ts
+++ cshop/src/app/products/products.component.ts
@@ -1,3 +1,5 @@
+import { Observable } from 'rxjs/Observable';
+import { ActivatedRoute } from '@angular/router';
 import { CategoryService } from './../category.service';
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
@@ -9,11 +11,26 @@ import { ProductService } from '../services/product.service';
 })
 export class ProductsComponent {
 
-  products$;
+  products$ = [];
   categories$;
+  category: string;
+  filteredProducts;
 
-  constructor(private productService: ProductService, categoryService: CategoryService) {
-    this.products$ = productService.getAll().valueChanges();
+  constructor(
+    route: ActivatedRoute,
+    private productService: ProductService, 
+    categoryService: CategoryService) {
+    //this.products$ = productService.getAll().valueChanges().subscribe;
     this.categories$ = categoryService.getCategories();
+
+    this.productService.getAll().snapshotChanges().subscribe(products => this.filteredProducts =this.products$= products);
+
+
+    route.queryParamMap.subscribe(params =>{
+      this.category = params.get('category');
+      this.filteredProducts = (this.category) ?
+        this.products$.filter(p => p.payload.val().category === this.category) :
+        this.products$;
+    });
    }
 }
